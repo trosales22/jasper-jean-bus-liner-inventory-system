a22a8e718380ff89edadd820a142bf6fbf7b2db0
Tristan Rosales <tristanrosales0@gmail.com>
2019-11-11 22:47:28 +0800

    Remove logout.

--- application/views/pages/topbar.php
+++ application/views/pages/topbar.php
@@ -79,18 +79,6 @@
 	  <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo "USER"; ?></span>
 	  <img class="img-profile rounded-circle" src="<?php echo base_url(); ?>static/images/logo.png">
 	</a>
-	<!-- Dropdown - User Information -->
-	<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-	  <a class="dropdown-item" href="#">
-		<i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-		Profile
-	  </a>
-	  <div class="dropdown-divider"></div>
-	  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-		<i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-		Logout
-	  </a>
-	</div>
   </li>
 
 </ul>
